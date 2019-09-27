0ebcf7d945840fa68ec272c75e71ead5ab3daf0c
Elias Santistevan <elias.santistevan@sparkfun.local>
2019-09-27 12:56:41 -0600

    Initial commit - file structure mostly

---
+++ DESCRIPTION.rst
@@ -0,0 +1,4 @@
+Qwiic CCS811 Sensor
+==========================
+
+This package provdes support for the SparkFun qwiic CCS811 sensor breakout.
---
+++ setup.py
@@ -0,0 +1,100 @@
+#------------------------------------------------------------------------
+#
+# This is a python install script written for qwiic python package.
+#
+# Written by  SparkFun Electronics, May 2019
+#
+# This python library supports the SparkFun Electroncis qwiic
+# ecosystem, providing an plaform indepenant interface to the
+# I2C bus.
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+#
+# Do you like this library? Help support SparkFun. Buy a board!
+#
+#==================================================================================
+# Copyright (c) 2019 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#==================================================================================
+
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from os import path
+import io
+
+here = path.abspath(path.dirname(__file__))
+
+# get the log description
+with io.open(path.join(here, "DESCRIPTION.rst"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+
+    name='sparkfun_qwiic_gps_ublox',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='0.9.4',
+
+    description='SparkFun Electronics qwiic gps Ublox',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://www.sparkfun.com/gps', #Temp place holder
+
+    # Author details
+    author='SparkFun Electronics',
+    author_email='info@sparkfun.com',
+
+    install_requires=['sparkfun_qwiic_i2c'],
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # What does your project relate to?
+    keywords='electronics, maker',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    py_modules=["qwiic_gps_ublox"],
+
+)
