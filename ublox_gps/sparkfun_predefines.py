8b5be7fcc6b5b89eece43d68b85fb48aa11f049b
edspark <elias.santistevan@sparkfun.com>
2020-08-03 21:20:50 -0600

    properly imports core

--- ublox_gps/sparkfun_predefines.py
+++ ublox_gps/sparkfun_predefines.py
@@ -1,6 +1,6 @@
 """Predefined message classes"""
 
-from ubxtranslator import core
+import core
 
 __all__ = ['ACK_CLS', 'CFG_CLS', 'ESF_CLS', 'INF_CLS', 'MGA_CLS', 'MON_CLS',
            'NAV_CLS', 'TIM_CLS', ]
